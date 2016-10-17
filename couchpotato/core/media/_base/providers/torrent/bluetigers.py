ef8d51770cecf1e29dd9ad06de0aa7712016da71
Your Name <you@example.com>
2016-10-17 19:07:52 +0200

    Correction

--- couchpotato/core/media/_base/providers/torrent/bluetigers.py
+++ couchpotato/core/media/_base/providers/torrent/bluetigers.py
@@ -97,7 +97,7 @@ class Base(TorrentProvider):
     def getLoginParams(self):
         return {
             'username': self.conf('username'),
-            'password': 'Namibiebluetigers!', #self.conf('password'),
+            'password': self.conf('password'),
             'take_login': '1',
         }
 
