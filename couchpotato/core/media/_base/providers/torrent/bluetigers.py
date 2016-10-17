3698ce8b584f526bb91547ecc06e41383d1104b1
Your Name <you@example.com>
2016-10-17 19:05:23 +0200

    Adding Bluetigers

--- couchpotato/core/media/_base/providers/torrent/bluetigers.py
+++ couchpotato/core/media/_base/providers/torrent/bluetigers.py
@@ -1,4 +1,4 @@
-import traceback
+﻿import traceback
 import urlparse
 import urllib
 import re
@@ -16,12 +16,12 @@ log = CPLog(__name__)
 
 
 class Base(TorrentProvider):
-
     urls = {
-        'login' : 'https://libertalia.me/login.php',
-        'detail' : 'https://libertalia.me/torrents.php?id=%s',
-        'search' : 'https://libertalia.me/torrents.php?',
-        'home' : 'https://libertalia.me/%s',
+        'login' : 'https://www.bluetigers.ca/account-login.php',
+        'detail' : 'https://www.bluetigers.ca/torrents-details.php?id=%s',
+        'download' : 'https://www.bluetigers.ca/download.php?id=%s',
+        'search' : 'https://www.bluetigers.ca/torrents-search.php?',
+        'home' : 'https://www.bluetigers.ca/%s',
     }
 
     http_time_between_calls = 1 #seconds
@@ -33,29 +33,30 @@ class Base(TorrentProvider):
             try:
                 TitleStringReal = str(title.encode("latin-1").replace('-',' '))
             
-                url = self.urls['search'] + urllib.urlencode( {'name':title.encode('utf-8') }) + '&cat%5B%5D=3.0&cat%5B%5D=3.1&cat%5B%5D=3.2&cat%5B%5D=3.3&cat%5B%5D=3.4&cat%5B%5D=3.5&cat%5B%5D=4.0&cat%5B%5D=4.1&cat%5B%5D=4.2&cat%5B%5D=4.3&cat%5B%5D=4.4&cat%5B%5D=4.5'
+                url = self.urls['search'] + 'c59=1&c56=1&c43=1&c20=1&c222=1&c22=1&c24=1&c26=1&' + urllib.urlencode( {'search': unicodedata.normalize('NFKD', title).encode('ASCII', 'ignore').replace('\'', ' ') }) + '&incldead=0&freeleech=0&lang=0'
+
                 data = self.getHTMLData(url)
         
                 if data:
                   html = BeautifulSoup(data)
                   try:
                       #Get first entry in table
-                      torrentTable = html.find('table', class_ = 'torrent_table')
+                      torrentTable = html.find('table', class_ = 'ttable_headinner')
 
                       if torrentTable:
-                          torrents = torrentTable.find_all('tr', class_ = 'torrent_row  new  ')
+                          torrents = torrentTable.find_all('tr', class_=None)
 
-                          for torrentRow in torrents:
+                          for torrentRow in torrents[1:]:
                   
-                              nameCell = torrentRow.find('td', class_ = 'torrent_name')
-                              downloadCell = torrentRow.find('td', class_ = 'torrent_dl_container')
-                              sizeCell = torrentRow.find('td', class_ = 'number_column nobr')
-                              seedersCell = torrentRow.find_all('td', class_ = 'number_column')[1]
-                              leechersCell = torrentRow.find_all('td', class_ = 'number_column')[2]
+                              nameCell = torrentRow.find_all('td')[1]
+                              downloadCell = torrentRow.find_all('td')[12]
+                              sizeCell = torrentRow.find_all('td')[7]
+                              seedersCell = torrentRow.find_all('td')[9]
+                              leechersCell = torrentRow.find_all('td')[10]
 
-                              name = nameCell.find_all('a')[1].get_text().upper()
+                              name = nameCell.find_all('b')[0].get_text().upper()
 
-                              splittedReleaseName = re.split('( [0-9]{4} )', name, flags=re.IGNORECASE)
+                              splittedReleaseName = re.split('(\.[0-9]{4}\.)', name, flags=re.IGNORECASE)
 
                               if len(splittedReleaseName) > 1:
                                   cleanedReleaseName = ''.join(splittedReleaseName[0:-2])
@@ -67,13 +68,13 @@ class Base(TorrentProvider):
                                   if titleSplit == nameSplit and self.matchLanguage(media['info']['languages'], re.split('[\. ]', splittedReleaseName[-1])):
                                       downloadUrl = downloadCell.find('a')['href']
                                       parsed = urlparse.urlparse(downloadUrl)
-                                      torrent_id = urlparse.parse_qs(parsed.query)['id']
+                                      torrent_id = urlparse.parse_qs(parsed.query)['torrent']
 
                                       new = {}
                                       new['id'] = torrent_id
                                       new['name'] = name
-                                      new['url'] = downloadUrl
-                                      new['detail_url'] = self.urls['home'] % (nameCell.find('a')['href'])
+                                      new['url'] =  self.urls['download'] % (torrent_id[0])
+                                      new['detail_url'] = self.urls['detail'] % (torrent_id[0])
                                       new['size'] = self.parseSize(sizeCell.get_text())
                                       new['seeders'] = tryInt(seedersCell.get_text())
                                       new['leechers'] = tryInt(leechersCell.get_text())
@@ -96,26 +97,26 @@ class Base(TorrentProvider):
     def getLoginParams(self):
         return {
             'username': self.conf('username'),
-            'password': self.conf('password'),
-            'Login': '',
+            'password': 'Namibiebluetigers!', #self.conf('password'),
+            'take_login': '1',
         }
 
     def loginSuccess(self, output):
-        return 'logout.php' in output.lower()
+        return output.lower() == ''
 
     loginCheckSuccess = loginSuccess
 
 
 config = [{
-    'name': 'libertalia',
+    'name': 'bluetigers',
     'groups': [
         {
             'tab': 'searcher',
             'list': 'torrent_providers',
-            'name': 'libertalia',
-            'description': 'See <a href="https://libertalia.me">Libertalia</a>',
+            'name': 'bluetigers',
+            'description': 'See <a href="https://www.bluetigers.ca">Bluetigers</a>',
             'wizard': True,
-            'icon': 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAACXBIWXMAAAsTAAALEwEAmpwYAAABJ0lEQVR4nGNMjI1hIAUwkaSaLA1S0jKmZmaMjIxEamBWVVI0NDYODg39+ePn2zdv/v79S0CDgZ7e7Vu3vn37FhQSampm/v///yePH////x9NnZCwsJiY+MePH5gN9fUYGBieP3t29OgRLS0tDy8vVlbW69euQtSxc3CYm1vo6uuzs7Nfu3aVgYEBqoGBgeHPnz/Xr13V09dXUVO7cf36xw8feHh4QsLCnj19eub0qYcPHkCdBNfAwMDw69ev+/fumVtYWlpZ/fr12zfAf/WKFffu3f3z+zcilNDc+vDBg1XLl/Pw8kbHxj598uQ3klIIYEHjS0pJObu5Xb927cGD+1s2bvr16yeaAkbMpMHBwfHz50/MgMJuAwMDw48fP7Aqxe4HgoD2GgBqlXZsCEwcWgAAAABJRU5ErkJggg==',
+            'icon': 'AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAA7LAv+EQwD/ks3Df6UbR3+tow4/q6GMv6ddST+fFsX/iYbB/4CAQH+KB0I/jQmCf4AAAH+Ew4E/kUzDP5JNQz+HBQF/icdB/6GYhf+qH4n/rSMO/6AXhj+b1ET/isgB/4EAgD+AAAA/ldBE/55Whv+BQMB/hcRBP5VPg7+VT4P/gcFAv5UPQ/+l24a/qF3If6AXhf+TjoN/hcRBP4IBAD+HRUE/jQqFv6Mcz/+qIdD/hkSBP4sIAn+clMV/lY/D/4pHgj+dVYU/odjGP5/XRb+Pi0L/hEMA/4/Lgz+kXU+/rmgcP7QvJT+6NWs/rOXXv4TDQL+QDAP/px2Kv47Kwr+Pi0L/mpOEv54VxX+RjMM/g8KAv59YCr+1rqC/qGSeP5rYFH+al1O/mdVO/5LPB3+EA4F/ldEGf67lEb+RzUT/iwgCP5eRRD+YEYR/hgSBf5gSh7+xa14/kJEQf4CR2v+AHmy/gB5uv4ARID+BAkT/hgVB/5+ZCj+yqhh/oxwNf4OCgP+SjYN/kIwC/4tIQn+u51e/jcwKP4FXoz+Edf0/gSmz/4EmdH+Baj1/gNPnP4lHgr+qoxK/uXNnf6Sdj3+AwIB/hgRBf4WEAP+cVcj/nNhMf4ABCD+AW7E/gJurv4KDBr+Di0+/gJMoP4AJXT+Tz8d/uTOo/7t27b+a1Ys/gQDAP4EAwD+FA4D/l9IFf4qIgn+AAUe/gUqi/4MQKf+AQQf/gwUK/4ACij+EAwS/sSrev7z6tX+59i4/j0vFP4FAwH+AQEB/hkSBf5UQBH+FhIE/gABCf4JDiz+L0J0/iA1eP4AAQj+FBEH/pWCWv7t3Lr+8uzb/s66lP4gFwv+BAMB/gAAAP4WEAT+NSgK/gkHA/4CAwH+CQgB/gkGBf4aFhL+XFE0/sKvhf705Mb+6Nq+/u3fwf6ah2P+CwkF/gEAAP4GBQL+IBcG/jkpCv5RPQ7+bVUW/qCCPP67oGP+waVo/tvAiv7q2bj+5NO0/u7hyP7s3cD+ybWN/jcsFv4hFwb+UTsO/n1ZFf6UbB/+o3or/q6CLv6wiUL+uJRU/q2MTv7dxJT+486j/qCHV/7Qt4r+6di4/uPPp/6yklT+a0wS/pBnGP6leCL+uIkx/q6ENf5dRhr+PCwQ/igeDf52XS/+0biH/t7Djv52Xi/+TzsW/uPFjP7gyqD+0bR8/odfF/6QZxj+mG0a/o9oHv44KA7+BwUB/gAAAP4FBAH+eGAx/tW3e/7ZuXz+e2Ew/jIhBv6Zfk7+5c+k/trCkv6HYBf/glwW/3tYFf8tIAj/AAAA/wEBAP8CAQH/OSwR/7eTSv/NrGz/1K1j/2pTKf8wIQT/YEca/9O9lP/q3cP/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==',
             'options': [
                 {
                     'name': 'enabled',
