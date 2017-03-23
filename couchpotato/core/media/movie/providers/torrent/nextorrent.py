10915856ee74e8e28a32e10c8c28ba3ddc35712a
cyberden <cyberden91@gmail.com>
2017-03-23 20:26:25 +0100

    Ajout provider (#13) (#14)
    * Ajout nextorrent
    
    * Ajout nextorrent

---
+++ couchpotato/core/media/movie/providers/torrent/nextorrent.py
@@ -0,0 +1,11 @@
+﻿from couchpotato.core.logger import CPLog
+from couchpotato.core.media._base.providers.torrent.cpasbien import Base
+from couchpotato.core.media.movie.providers.base import MovieProvider
+
+log = CPLog(__name__)
+
+autoload = 'nextorrent'
+
+
+class nextorrent(MovieProvider, Base):
+    pass
