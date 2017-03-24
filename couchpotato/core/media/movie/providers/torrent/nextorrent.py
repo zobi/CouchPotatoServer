c497b234b9411907224b49cc497d8afb7eb8c413
Foncekar <fr.frimont@gmail.com>
2017-03-24 16:01:57 +0100

    Update nextorrent.py (#15)

--- couchpotato/core/media/movie/providers/torrent/nextorrent.py
+++ couchpotato/core/media/movie/providers/torrent/nextorrent.py
@@ -1,5 +1,5 @@
 ﻿from couchpotato.core.logger import CPLog
-from couchpotato.core.media._base.providers.torrent.cpasbien import Base
+from couchpotato.core.media._base.providers.torrent.nextorrent import Base
 from couchpotato.core.media.movie.providers.base import MovieProvider
 
 log = CPLog(__name__)
